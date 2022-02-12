ea5170930c8092c61a297261d24537d377efe5e0
pasturl <65410778+pasturl@users.noreply.github.com>
2022-02-12 16:16:57 +0100

    Update app.py

--- dashboard_streamlit/app.py
+++ dashboard_streamlit/app.py
@@ -95,66 +95,66 @@ if uploaded_file is not None:
     df_weekly = pd.read_csv(uploaded_file)
     df_weekly["yw"] = df_weekly["yw"].astype(str)
         
-continents = list(df_weekly.sort_values(by=['continent'])['continent'].unique())
+    continents = list(df_weekly.sort_values(by=['continent'])['continent'].unique())
 
 
-continent = st.sidebar.selectbox('Continent Selection', continents)
-df_plot = filter_data(df_weekly, continent).reset_index(drop=True)
-locations = list(df_plot.sort_values(by=['location'])['location'].unique())
-country = st.sidebar.selectbox('Country Selection', locations)
-df_country = filter_data_country(df_plot, country).reset_index(drop=True)
+    continent = st.sidebar.selectbox('Continent Selection', continents)
+    df_plot = filter_data(df_weekly, continent).reset_index(drop=True)
+    locations = list(df_plot.sort_values(by=['location'])['location'].unique())
+    country = st.sidebar.selectbox('Country Selection', locations)
+    df_country = filter_data_country(df_plot, country).reset_index(drop=True)
 
-kpis_ts = ['new_cases', 'new_cases_smoothed', 'new_deaths',
-           'new_deaths_smoothed', 'new_cases_per_million', 'new_cases_smoothed_per_million',
-           'new_deaths_per_million', 'new_deaths_smoothed_per_million', 'hosp_patients',
-           'hosp_patients_per_million', 'weekly_icu_admissions',
-           'weekly_icu_admissions_per_million', 'new_tests',
-           'new_tests_per_thousand', 'positive_rate',
-           'people_vaccinated', 'people_fully_vaccinated',
-           'new_vaccinations', 'total_vaccinations_per_hundred',
-           'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred',
-           'stringency_index']
-kpi_ts = st.sidebar.selectbox('KPI Time lapse', kpis_ts)
+    kpis_ts = ['new_cases', 'new_cases_smoothed', 'new_deaths',
+               'new_deaths_smoothed', 'new_cases_per_million', 'new_cases_smoothed_per_million',
+               'new_deaths_per_million', 'new_deaths_smoothed_per_million', 'hosp_patients',
+               'hosp_patients_per_million', 'weekly_icu_admissions',
+               'weekly_icu_admissions_per_million', 'new_tests',
+               'new_tests_per_thousand', 'positive_rate',
+               'people_vaccinated', 'people_fully_vaccinated',
+               'new_vaccinations', 'total_vaccinations_per_hundred',
+               'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred',
+               'stringency_index']
+    kpi_ts = st.sidebar.selectbox('KPI Time lapse', kpis_ts)
 
-kpi_agg = ['total_cases', 'total_deaths','total_cases_per_million',
-           'total_deaths_per_million', 'hosp_patients',
-           'hosp_patients_per_million', 'weekly_icu_admissions',
-           'weekly_icu_admissions_per_million', 'total_tests',
-           'total_tests_per_thousand', 'total_vaccinations', 'people_vaccinated',
-           'people_fully_vaccinated', 'total_vaccinations_per_hundred',
-           'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred',
-           'stringency_index']
+    kpi_agg = ['total_cases', 'total_deaths','total_cases_per_million',
+               'total_deaths_per_million', 'hosp_patients',
+               'hosp_patients_per_million', 'weekly_icu_admissions',
+               'weekly_icu_admissions_per_million', 'total_tests',
+               'total_tests_per_thousand', 'total_vaccinations', 'people_vaccinated',
+               'people_fully_vaccinated', 'total_vaccinations_per_hundred',
+               'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred',
+               'stringency_index']
 
-kpi_agg = st.sidebar.selectbox('KPI aggregated metric', kpi_agg)
-
-
-df_total = agg_data(df_plot, kpi_agg)
-lat_foc = df_plot["lat"].to_numpy().astype(float).mean()
-lon_foc = df_plot["lon"].to_numpy().astype(float).mean()
-fig = plot_continent(df_plot, lat_foc, lon_foc, kpi_ts)
-expander_time = st.expander("Map time-lapse Covid evolution")
-
-# display streamlit map
-expander_time.plotly_chart(fig)
-my_expander_agg = st.expander("Map aggregated metric")
-
-fig2 = plot_map(df_total, mapbox_access_token, lat_foc, lon_foc, kpi_agg)
-my_expander_agg.plotly_chart(fig2)
-
-my_expander_ts = st.expander("Time series chart")
-fig3 = plot_ts(df_country, kpi_ts)
-my_expander_ts.plotly_chart(fig3)
+    kpi_agg = st.sidebar.selectbox('KPI aggregated metric', kpi_agg)
 
 
-my_expander_table = st.expander("Summary table")
-# CSS to inject contained in a string
-hide_dataframe_row_index = """
-            <style>
-            .row_heading.level0 {display:none}
-            .blank {display:none}
-            </style>
-            """
+    df_total = agg_data(df_plot, kpi_agg)
+    lat_foc = df_plot["lat"].to_numpy().astype(float).mean()
+    lon_foc = df_plot["lon"].to_numpy().astype(float).mean()
+    fig = plot_continent(df_plot, lat_foc, lon_foc, kpi_ts)
+    expander_time = st.expander("Map time-lapse Covid evolution")
 
-# Inject CSS with Markdown
-st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
-my_expander_table.dataframe(df_plot)
+    # display streamlit map
+    expander_time.plotly_chart(fig)
+    my_expander_agg = st.expander("Map aggregated metric")
+
+    fig2 = plot_map(df_total, mapbox_access_token, lat_foc, lon_foc, kpi_agg)
+    my_expander_agg.plotly_chart(fig2)
+
+    my_expander_ts = st.expander("Time series chart")
+    fig3 = plot_ts(df_country, kpi_ts)
+    my_expander_ts.plotly_chart(fig3)
+
+
+    my_expander_table = st.expander("Summary table")
+    # CSS to inject contained in a string
+    hide_dataframe_row_index = """
+                <style>
+                .row_heading.level0 {display:none}
+                .blank {display:none}
+                </style>
+                """
+
+    # Inject CSS with Markdown
+    st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+    my_expander_table.dataframe(df_plot)
