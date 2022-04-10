1e4c420ae864c7b01cc669495ea4abccad064c54
pasturl <lucas.pastur@gmail.com>
2022-04-10 11:42:35 +0200

    Remove upload data

--- dashboard_streamlit/app.py
+++ dashboard_streamlit/app.py
@@ -7,6 +7,11 @@ from sklearn.preprocessing import MinMaxScaler
 st.set_page_config(layout="wide")
 mapbox_access_token = st.secrets["TOKEN"]
 
+@st.cache
+def read_data_weekly():
+    df = pd.read_csv("owid-covid-data-gps-weekly.csv")
+    df["yw"] = df["yw"].astype(str)
+    return df
 
 @st.cache
 def filter_data(df, continent):
@@ -84,77 +89,78 @@ def plot_map(df, mapbox_access_token, lat_foc, lon_foc, kpi_agg):
 
 
 def plot_ts(df, kpi_ts):
-    fig = px.line(df, x='yw', y=kpi_ts)
+    fig = px.line(df, x='yw', y=kpi_ts, labels=dict(yw="Date (year-week)"))
     fig.update_layout(
         width=1000,
         height=500)
     return fig
-# df = read_data()
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    df_weekly = pd.read_csv(uploaded_file)
-    df_weekly["yw"] = df_weekly["yw"].astype(str)
+
+df_weekly = read_data_weekly()
+df_weekly["yw"] = df_weekly["yw"].astype(str)
         
-    continents = list(df_weekly.sort_values(by=['continent'])['continent'].unique())
+continents = list(df_weekly.sort_values(by=['continent'])['continent'].unique())
+
+st.header('Streamlit app to visualize covid data from https://github.com/owid/covid-19-data')
+st.subheader('Visualization of evolution of main pandemic parameters'
+             ' (deaths, new cases, hospitalizations, vaccinations, etc).')
+st.sidebar.subheader('Filters and KPI selection')
+continent = st.sidebar.selectbox('Continent', continents)
+df_plot = filter_data(df_weekly, continent).reset_index(drop=True)
+locations = list(df_plot.sort_values(by=['location'])['location'].unique())
+country = st.sidebar.selectbox('Country', locations)
+df_country = filter_data_country(df_plot, country).reset_index(drop=True)
+
+kpis_ts = ['new_cases', 'new_cases_smoothed', 'new_deaths',
+           'new_deaths_smoothed', 'new_cases_per_million', 'new_cases_smoothed_per_million',
+           'new_deaths_per_million', 'new_deaths_smoothed_per_million', 'hosp_patients',
+           'hosp_patients_per_million', 'weekly_icu_admissions',
+           'weekly_icu_admissions_per_million', 'new_tests',
+           'new_tests_per_thousand', 'positive_rate',
+           'people_vaccinated', 'people_fully_vaccinated',
+           'new_vaccinations', 'total_vaccinations_per_hundred',
+           'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred',
+           'stringency_index']
+kpi_ts = st.sidebar.selectbox('KPI Time lapse', kpis_ts)
+
+kpi_agg = ['total_cases', 'total_deaths','total_cases_per_million',
+           'total_deaths_per_million', 'hosp_patients',
+           'hosp_patients_per_million', 'weekly_icu_admissions',
+           'weekly_icu_admissions_per_million', 'total_tests',
+           'total_tests_per_thousand', 'total_vaccinations', 'people_vaccinated',
+           'people_fully_vaccinated', 'total_vaccinations_per_hundred',
+           'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred',
+           'stringency_index']
+
+kpi_agg = st.sidebar.selectbox('KPI aggregated metric', kpi_agg)
 
 
-    continent = st.sidebar.selectbox('Continent Selection', continents)
-    df_plot = filter_data(df_weekly, continent).reset_index(drop=True)
-    locations = list(df_plot.sort_values(by=['location'])['location'].unique())
-    country = st.sidebar.selectbox('Country Selection', locations)
-    df_country = filter_data_country(df_plot, country).reset_index(drop=True)
+df_total = agg_data(df_plot, kpi_agg)
+lat_foc = df_plot["lat"].to_numpy().astype(float).mean()
+lon_foc = df_plot["lon"].to_numpy().astype(float).mean()
+fig = plot_continent(df_plot, lat_foc, lon_foc, kpi_ts)
+expander_time = st.expander("Dynamic map with time-lapse covid evolution")
 
-    kpis_ts = ['new_cases', 'new_cases_smoothed', 'new_deaths',
-               'new_deaths_smoothed', 'new_cases_per_million', 'new_cases_smoothed_per_million',
-               'new_deaths_per_million', 'new_deaths_smoothed_per_million', 'hosp_patients',
-               'hosp_patients_per_million', 'weekly_icu_admissions',
-               'weekly_icu_admissions_per_million', 'new_tests',
-               'new_tests_per_thousand', 'positive_rate',
-               'people_vaccinated', 'people_fully_vaccinated',
-               'new_vaccinations', 'total_vaccinations_per_hundred',
-               'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred',
-               'stringency_index']
-    kpi_ts = st.sidebar.selectbox('KPI Time lapse', kpis_ts)
+# display streamlit map
+expander_time.plotly_chart(fig)
+my_expander_agg = st.expander("Static map with aggregated KPI")
 
-    kpi_agg = ['total_cases', 'total_deaths','total_cases_per_million',
-               'total_deaths_per_million', 'hosp_patients',
-               'hosp_patients_per_million', 'weekly_icu_admissions',
-               'weekly_icu_admissions_per_million', 'total_tests',
-               'total_tests_per_thousand', 'total_vaccinations', 'people_vaccinated',
-               'people_fully_vaccinated', 'total_vaccinations_per_hundred',
-               'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred',
-               'stringency_index']
+fig2 = plot_map(df_total, mapbox_access_token, lat_foc, lon_foc, kpi_agg)
+my_expander_agg.plotly_chart(fig2)
 
-    kpi_agg = st.sidebar.selectbox('KPI aggregated metric', kpi_agg)
+my_expander_ts = st.expander("Time series chart")
+fig3 = plot_ts(df_country, kpi_ts)
+my_expander_ts.plotly_chart(fig3)
 
 
-    df_total = agg_data(df_plot, kpi_agg)
-    lat_foc = df_plot["lat"].to_numpy().astype(float).mean()
-    lon_foc = df_plot["lon"].to_numpy().astype(float).mean()
-    fig = plot_continent(df_plot, lat_foc, lon_foc, kpi_ts)
-    expander_time = st.expander("Map time-lapse Covid evolution")
+my_expander_table = st.expander("Summary table")
+# CSS to inject contained in a string
+hide_dataframe_row_index = """
+            <style>
+            .row_heading.level0 {display:none}
+            .blank {display:none}
+            </style>
+            """
 
-    # display streamlit map
-    expander_time.plotly_chart(fig)
-    my_expander_agg = st.expander("Map aggregated metric")
-
-    fig2 = plot_map(df_total, mapbox_access_token, lat_foc, lon_foc, kpi_agg)
-    my_expander_agg.plotly_chart(fig2)
-
-    my_expander_ts = st.expander("Time series chart")
-    fig3 = plot_ts(df_country, kpi_ts)
-    my_expander_ts.plotly_chart(fig3)
-
-
-    my_expander_table = st.expander("Summary table")
-    # CSS to inject contained in a string
-    hide_dataframe_row_index = """
-                <style>
-                .row_heading.level0 {display:none}
-                .blank {display:none}
-                </style>
-                """
-
-    # Inject CSS with Markdown
-    st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
-    my_expander_table.dataframe(df_plot)
+# Inject CSS with Markdown
+st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+my_expander_table.dataframe(df_plot)
